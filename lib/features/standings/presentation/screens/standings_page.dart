0238d44c28bb561544c04002791587def90ee638
Krmsh1n5 <Dima7715@icloud.com>
2025-01-15 16:03:17 +0400

    Added simple UI for Standungs Page

---
+++ lib/features/standings/presentation/screens/standings_page.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:u_standings/features/standings/presentation/widgets/appbar.dart';
+
+class StandingsPage extends StatelessWidget {
+  const StandingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: _buildAppbar(),
+        body: Container(
+          width: double.infinity,
+          padding:
+              // Platform.isIOS
+              //     ? EdgeInsets.zero : // Use only SafeArea's padding on iOS
+              EdgeInsets.only(
+            // Add custom padding on Android
+            left: 16.h,
+            top: 68.h,
+            right: 16.h,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                _buildTitleWidget(),
+                _buildCompletedExamCreditsWidget(),
+                SizedBox(height: 4.h),
+                _buildHeadingAndSubheadingText(),
+                SizedBox(height: 4.h),
+                _buildButton(),
+                SizedBox(height: 4.h),
+                _buildStandingsTable(),
+                SizedBox(height: 10.h),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildAppbar() {
+    return CustomAppBar(
+      title: Padding(
+        padding: EdgeInsets.only(left: 12.h),
+        child: Text(
+          'Standings',
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.refresh_rounded),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  _buildTitleWidget() {
+    return SizedBox(
+      width: double.maxFinite,
+      height: 48.h,
+      child: Text(
+        "Explore the leaderboard rankings!",
+        style: TextStyle(
+          fontSize: 20.sp,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  _buildCompletedExamCreditsWidget() {
+    // Example data for completed and total credits
+    double completedCredits = 18;
+    double totalCredits = 30;
+
+    // Calculate the percentage of completed credits
+    double percentageCompleted = completedCredits / totalCredits;
+
+    return SizedBox(
+      width: double.infinity,
+      height: 55.h,
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 20.h,
+            child: Text(
+              "Completed Exam Credits",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 8.h,
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: (percentageCompleted * 100).toInt(),
+                child: Container(
+                  height: 25.h,
+                  color: Colors.blue[400],
+                  alignment: Alignment.center,
+                  child: Text(
+                    "$completedCredits / $totalCredits",
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: ((1 - percentageCompleted) * 100).toInt(),
+                child: Container(
+                  height: 25.h,
+                  color: Colors.grey[700],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildHeadingAndSubheadingText() {
+    return SizedBox(
+      height: 40.h,
+      width: double.maxFinite,
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.maxFinite,
+            height: 20.h,
+            child: Text(
+              "Leaderboard",
+            ),
+          ),
+          SizedBox(
+            height: 4.h,
+            width: double.maxFinite,
+          ),
+          SizedBox(
+            width: double.maxFinite,
+            height: 16.h,
+            child: Text(
+              "Select a semester and a cohort",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildButton() {
+    return SizedBox(
+      height: 32.h,
+      width: double.maxFinite,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text("CS 2023"),
+      ),
+    );
+  }
+
+  _buildStandingsTable() {
+    return Container(width: double.maxFinite, height: 437.h, color: Colors.red);
+  }
+}
